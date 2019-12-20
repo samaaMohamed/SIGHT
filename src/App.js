c86a6a47707fd4edfed6bc0aab988401fccb01c9
samaaMohamed <samaamohammed15@gmail.com>
2019-12-20 12:06:10 -0800

    stracture

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from './componants/Home/Navbar';
+
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+               <NavBar></NavBar> 
+            </div>
+        )
+    }
+}
