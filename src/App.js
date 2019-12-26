cab77ba159698994ef7fdda6c5ab596c4cd8be2c
samaaMohamed <samaamohammed15@gmail.com>
2019-12-26 04:54:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import NavBar from "./pages/Home/Navbar";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar></NavBar>
+        
       </div>
     );
   }
